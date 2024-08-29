ce3aff2aedee8acf5c90b512537f419bcfe7c6f1
martinjohnm <johnmmartin74@gmail.com>
2024-08-30 00:06:21 +0530

    home page completed

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { Hero } from "../components/Hero"
+import { Navbar } from "../components/Navbar"
+import { Steps } from "../components/Steps"
+
+
+
+
+export const Home = () => {
+    return <div>
+    
+        <Navbar/>
+        <Hero/>
+        <Steps/>
+    
+    </div>
+
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,4 +8,6 @@ export default {
     extend: {},
   },
   plugins: [],
+  darkMode: ['selector', '[data-mode="dark"]'],
+
 }
