dfb7be8c4bee05b94e69966c95c29522e3b9a317
martinjohnm <johnmmartin74@gmail.com>
2024-08-31 11:23:46 +0530

    sonner toast added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import { Hero } from "../components/Hero"
 import { Navbar } from "../components/Navbar"
 import { Steps } from "../components/Steps"
-
-
+import { toast } from "sonner"
 
 
 export const Home = () => {
+
+   
     return <div className="bg-slate-100">
     
         <Navbar/>
         <Hero/>
+        <button onClick={() => toast.success('My first toast')} className="bg-red-300 rounded-sm p-2">
+        Give me a toast
+        </button>
         <Steps/>
+        
     
     </div>
 
