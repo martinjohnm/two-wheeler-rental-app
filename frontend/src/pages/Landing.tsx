29a9355d149df800dae21856bcd1c7a54291423f
martinjohnm <johnmmartin74@gmail.com>
2024-10-19 01:11:39 +0530

    landing page redesigned 2

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,11 +1,9 @@
-import { useRecoilValue } from "recoil"
-import { userAtom } from "../store/atoms"
+
 import { Link } from "react-router-dom"
 
 export const Landing = () => {
 
-    const user = useRecoilValue(userAtom)
-   
+
     return <div className="bg-[url('bgimage.png')] bg-no-repeat bg-cover h-screen py-40">
 {/*     
         <Navbar user={user}/>
