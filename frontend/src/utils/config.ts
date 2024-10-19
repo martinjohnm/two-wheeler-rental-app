790e2272dfccbba962819ae9d06888f00661fa5f
martinjohnm <johnmmartin74@gmail.com>
2024-10-19 18:28:31 +0530

    Backend url in frontend is put in env

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,6 +1,6 @@
 
 
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 export const USER_TOKEN = "twowheelerapp-user"
 export const ADMIN_TOKEN = "twowheelerapp-admin"
